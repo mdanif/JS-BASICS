7549dfdd3148626b7b59f1c5223c1fd491a9bd7c
Irshad Ali <irshadali786143@gmail.com>
2019-12-16 15:39:46 +0530

    Done With 1 Hour JS

---
+++ 8.js
@@ -0,0 +1,18 @@
+// A function is set of statements for Performing a task or Calculating a value
+ 
+// Performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName); // + for concadinate two strings
+}
+
+// greet('irshad', 'ali');
+
+// Calculating a value
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
+
+
+
