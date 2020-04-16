ca194d883e6c6b6116aeb99c230a1b29865cd1b6
phonerefer <irshadali786143@gmail.com>
2020-04-16 15:07:49 +0530

    Coded Added

--- 11.js
+++ 11.js
@@ -1,5 +1,17 @@
-// Aithmetic Operators
+// Aithmetic Operators (=) is Assignment Operator
 let x = 10;
 let y = 3;
 
-console.log(x + y);
+console.log(x + y);  // (x+y) This is an exprassion
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y); // This is x power of y
+
+// Increment (++)
+console.log(x++);
+console.log(x);
+
+// Decrement (--)
+console.log(x--);
--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// Aithmetic Operators
-let x = 10;
-let y = 3;
-
-console.log(x + y);  // (x+y) This is an exprassion
-console.log(x - y);
-console.log(x * y);
-console.log(x / y);
-console.log(x % y);
-console.log(x ** y); // This is x power of y
-
-// Increment (++)
-console.log(++x);
-console.log(x++);
-
-// Decrement (--)
-console.log(--x);
-console.log(x--);
+let points = 10;
+let type = points >= 100 ? 'gold' : 'silver';
+console.log(type);
